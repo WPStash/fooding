46b57f984347c81b584481d67acf05304d883641
Cong Thien <luckystar.ictu90@gmail.com>
2016-07-08 13:21:48 +0700

    Query post issue closes #4

--- template-parts/content-staff.php
+++ template-parts/content-staff.php
@@ -14,7 +14,7 @@
             $tags = get_theme_mod( 'fooding_staff_picks_tags' );
             $number   = intval( get_theme_mod( 'number_staff_picks', 5 ) );
             $number   = ( 0 != $number ) ? $number : 5;
-            $args = array( 'posts_per_page' => $number , 'tag' => $tags , 'ignore_sticky_posts' => true );
+            $args = array( 'posts_per_page' => $number , 'tag' => $tags , 'ignore_sticky_posts' => true, 'meta_query' => array(array('key' => '_thumbnail_id')) );
             $staff_picks = new WP_Query( $args );
             ?>
 
